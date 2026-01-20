0ab1ca2e1a22e4c9101acb96e61ba73815cfb518
ZelalDev <zelall2222@gmail.com>
2026-01-20 16:01:14 +0300

    status feauture has added

--- RealEstate/main.swift
+++ RealEstate/main.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+enum Status{
+    case For_Rent
+    case For_Sell
+}
+
 protocol Building{
     var roomNumber:Int{get}
     var cost:Int{get set}
     var estateAgent:String{get set}
+    var status:Status{get set}
     func salesSummary()
 }
 
@@ -20,23 +26,26 @@ extension Building{
             How many rooms: \(roomNumber)
             The price: \(cost)$
             Estate Agent: \(estateAgent)
+            Status: \(status)
             """)
-    } 
+    }
 }
 
 struct House:Building{
     var roomNumber: Int
     var cost: Int
     var estateAgent: String
+    var status: Status
 }
 
 struct Office:Building{
     var roomNumber: Int
     var cost: Int
     var estateAgent: String
+    var status: Status
 }
 
 
-let house1 = House(roomNumber: 3, cost: 3450, estateAgent: "...")
+let house1 = House(roomNumber: 3, cost: 3450, estateAgent: "...", status: .For_Sell)
 
 house1.salesSummary()
